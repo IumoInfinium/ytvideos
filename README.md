2e57bbd60f7073828cd5450c57b34a910a8c045a
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-10 23:51:22 +0530

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Script to download youtube or youtube music videos
+
+## Who can use ?
+
+Anybody can use this freely however they want... But contributions are always welcome XD
+
+## How to use it ?
+
+1. Clone the Repository
+2. Run the script.py file
+3. Select your choice whether you want to download a single or a playlist.
+4. Then you will be asked for a link of that video or a playlist.
+5. Then input the location for saving the downloads to.
+6. All done... Enjoy your videos ;)
+
+## Contributions
+
+Contribute by raising issues or updates for making it more easy to use.
+
--- script.py
+++ script.py
@@ -1,3 +1,36 @@
+# import re
+# import os
+# try:
+#     os.system("pip install pytube")
+#     # from pytube import Playlist
+#     import pytube
+# except:
+#     print("\nError while installing PyTube")
+
+# print("Download type :")
+# print("1. Video")
+# print("2. Playlist")
+# ch=input("Select from above choices:")
+# if ch=="1" or ch=="Video":
+#     video_url=input("\nVideo url to download : ")
+#     yt = pytube.YouTube(video_url)
+#     cur_dir=input("\nLocation to save the video : ")
+#     yt.streams.get_highest_resolution().download(cur_dir)
+
+# elif ch=="2" or ch=="Playlist":
+#     playlist_to_download=input("\nPlaylist to download : ")
+
+#     playlist = pytube.Playlist(playlist_to_download)
+
+#     cur_dir=input("\nLocation to save the videos : ")
+
+#     for video in playlist.videos:
+#         print('\nDownloading : {} \nURL : {}'.format(video.title, video.watch_url))
+#         # video.streams.filter(type='video', progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(cur_dir)
+#         yt = pytube.YouTube(video.watch_url)
+#         yt.streams.get_highest_resolution().download(cur_dir)
+# else:
+#     print("Invalid input check again!")
 import re
 import os
 try:
@@ -18,13 +51,14 @@ if ch=="1" or ch=="Video":
     yt.streams.get_highest_resolution().download(cur_dir)
 
 elif ch=="2" or ch=="Playlist":
-    playlist_to_download=input("\nPlaylist to download : ")
+    playlist_to_download="https://www.youtube.com/playlist?list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q"
 
     playlist = pytube.Playlist(playlist_to_download)
 
     cur_dir=input("\nLocation to save the videos : ")
+    x = [ video for video in playlist.videos ]
 
-    for video in playlist.videos:
+    for video in x[64:]:
         print('\nDownloading : {} \nURL : {}'.format(video.title, video.watch_url))
         # video.streams.filter(type='video', progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(cur_dir)
         yt = pytube.YouTube(video.watch_url)
